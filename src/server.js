2ec2846c83b39995b30c7bce7a478dd958eb0192
Christine <christinezcollege@gmail.com>
2025-01-11 18:11:21 -0800

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "christinezcollege@gmail.com",
+    pass: "khjo pugr yode pxik"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "*christinezcollege@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
